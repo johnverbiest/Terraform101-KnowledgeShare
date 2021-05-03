ce3604955f60efc97866b2c9fc116f09fe250ab5
John Verbiest <john.verbiest@informat.be>
2021-05-03 10:46:15 +0200

    App Settings

--- Terraform/web.tf
+++ Terraform/web.tf
@@ -36,6 +36,20 @@ resource "azurerm_app_service" "awesomeTfDemo" {
     dotnet_framework_version = "v5.0"
   }
 
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"         = azurerm_application_insights.website.instrumentation_key
+    "ApplicationInsights.InstrumentationKey" = azurerm_application_insights.website.instrumentation_key
+    "storageAccount"                         = azurerm_storage_account.logStorage.primary_connection_string
+    "APPINSIGHTS_PORTALINFO"                 = "ASP.NET"
+    "APPINSIGHTS_PROFILERFEATURE_VERSION"    = "1.0.0"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=${azurerm_sql_server.sqlServer.fully_qualified_domain_name};Database=${azurerm_sql_database.AwesomeDatabase.name};User Id=${random_string.sqlUser.result};Password=${random_password.sqlPassword.result}"
+  }
+
   tags = {
     Provider = "Terraform"
     File = "web.tf"
